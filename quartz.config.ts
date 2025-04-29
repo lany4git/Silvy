df979eb199aa87dd8d75bdbf8ae405b72fbbd87f
lany4git <janeve.silva@gmail.com>
2025-04-29 03:58:01 -0500

    vault backup: 2025-04-29 03:58:01

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Spectral, monosphere",
+        header: "Gilda Display",
         body: "IBM Plex Mono",
         code: "IBM Plex Mono",
       },
